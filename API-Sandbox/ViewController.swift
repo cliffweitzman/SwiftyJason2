9e2d4ed7194af5c04838c99c0f37acfea471eeae
Cliff Weitzman <cliffweitzman@gmail.com>
2016-07-05 16:27:41 -0700

    load Movies done
     + truncating text + buttons to show link + shifting images + random
    movie display

--- API-Sandbox/ViewController.swift
+++ API-Sandbox/ViewController.swift
@@ -19,33 +19,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var releaseDateLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var posterImageView: UIImageView!
+    var link: String = ""
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        exerciseOne()
-        exerciseTwo()
-        exerciseThree()
-        
-        let apiToContact = "https://itunes.apple.com/us/rss/topmovies/limit=25/json"
-        // This code will call the iTunes top 25 movies endpoint listed above
-        Alamofire.request(.GET, apiToContact).validate().responseJSON() { response in
-            switch response.result {
-            case .Success:
-                if let value = response.result.value {
-                    let json = JSON(value)
-                    
-                    // Do what you need to with JSON here!
-                    // The rest is all boiler plate code you'll use for API requests
-                    
-                    
-                }
-            case .Failure(let error):
-                print(error)
-            }
-        }
+//        exerciseOne()
+//        exerciseTwo()
+//        exerciseThree()
+        loadMovie()
+  
     }
 
+
+    @IBAction func newMovie(sender: AnyObject) {
+        loadMovie()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -57,7 +48,55 @@ class ViewController: UIViewController {
     }
     
     @IBAction func viewOniTunesPressed(sender: AnyObject) {
+        print(link)
+        UIApplication.sharedApplication().openURL(NSURL(string: String(link))!)
+    }
+    
+    func loadMovie() {
         
+        let randomInt: Int = Int(arc4random_uniform(UInt32(25)))
+        var allMovies: [Movie] = []
+        
+        let apiToContact = "https://itunes.apple.com/us/rss/topmovies/limit=25/json"
+        // This code will call the iTunes top 25 movies endpoint listed above
+        Alamofire.request(.GET, apiToContact).validate().responseJSON() { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let moviesData = JSON(value)
+                    let allMoviesData = moviesData["feed"]["entry"].arrayValue
+                    /*
+                     Figure out a way to turn the allMoviesData array into Movie structs!
+                     */
+                    for movieJSON in allMoviesData {
+                        allMovies.append(Movie(json: movieJSON))
+                    }
+                    
+                    // Do what you need to with JSON here!
+                    // The rest is all boiler plate code you'll use for API requests
+                    self.movieTitleLabel.text = allMovies[randomInt].name
+                    
+                    var myText1 = allMovies[randomInt].rightsOwner
+                    if myText1.containsString(" All Rights Reserved") {
+                        myText1 = myText1.stringByReplacingOccurrencesOfString(" All Rights Reserved", withString: "")
+                    }
+                    if myText1 == "" {
+                        myText1 = "N/A"
+                    }
+                    self.rightsOwnerLabel.text = myText1
+                    
+                    let myText = allMovies[randomInt].releaseDate
+                    self.releaseDateLabel.text = String(myText.characters.dropLast(15))
+                    
+                    self.priceLabel.text = "$" + String(allMovies[randomInt].price)
+                    
+                    self.loadPoster(allMovies[randomInt].poster)
+                    self.link = allMovies[randomInt].link
+                }
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
     
 }
